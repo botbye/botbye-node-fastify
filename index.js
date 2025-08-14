f71ac74cde6b8503240de2181084a523a5d09562
botbye <accounts@botbye.com>
2025-08-14 12:23:05 +0300

    update requestInfo

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.init = init;
 exports.validateRequest = validateRequest;
 var botbye_node_core_1 = require("botbye-node-core");
-(0, botbye_node_core_1.initPackageInfo)({ name: "NodeJS-Fastify", version: "0.1.0" });
+(0, botbye_node_core_1.initPackageInfo)({ name: "NodeJS-Fastify", version: "0.1.1" });
 function init(options) {
     (0, botbye_node_core_1.init)(options);
     return validateRequest;
 }
 function validateRequest(options) {
-    var _a;
     var token = options.token, request = options.request, customFields = options.customFields;
     var headers = Object.entries(request.headers)
         .reduce(function (acc, _a) {
@@ -21,8 +20,6 @@ function validateRequest(options) {
         'request_uri': request.url,
         'request_method': request.method,
         'remote_addr': request.ip,
-        'server_port': String((_a = request.port) !== null && _a !== void 0 ? _a : "0000"),
-        'server_name': request.hostname || "",
     };
     return (0, botbye_node_core_1.validateRequest)({
         token: token,
