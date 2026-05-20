91b77de4f5787ab8f973d4cc6cef589b57cfa946
botbye <accounts@botbye.com>
2026-05-20 18:16:59 +0300

    V2

---
+++ src/constants.d.ts
@@ -0,0 +1,3 @@
+declare const MODULE_NAME = "FASTIFY";
+declare const MODULE_VERSION = "2.0.0";
+export { MODULE_NAME, MODULE_VERSION };
---
+++ src/index.d.ts
@@ -0,0 +1,91 @@
+import { type TModuleApi } from "@botbye/node-core";
+import { type FastifyRequest } from "fastify";
+type TFactoryOptions = {
+    module?: {
+        name?: string;
+        version?: string;
+    };
+};
+declare const factory: (options?: TFactoryOptions) => TModuleApi<FastifyRequest>;
+declare const init: (options: import("@botbye/node-core").TInitOptions) => void, evaluate: (event: {
+    type: "full";
+    request: {
+        ip: string;
+        headers: Record<string, string>;
+        token?: import("@botbye/node-core").TNullable<string>;
+        requestMethod?: import("@botbye/node-core").TNullable<string>;
+        requestUri?: import("@botbye/node-core").TNullable<string>;
+    } | {
+        request: FastifyRequest<import("fastify").RouteGenericInterface, import("fastify").RawServerDefault, import("http").IncomingMessage, import("fastify").FastifySchema, import("fastify").FastifyTypeProviderDefault, unknown, import("fastify").FastifyBaseLogger, import("fastify/types/type-provider").ResolveFastifyRequestType<import("fastify").FastifyTypeProviderDefault, import("fastify").FastifySchema, import("fastify").RouteGenericInterface>>;
+        token?: import("@botbye/node-core").TNullable<string>;
+    };
+    event: {
+        type: string;
+        status: import("@botbye/node-core").TUpstreamEventStatus;
+    };
+    user: {
+        accountId: string;
+        username?: import("@botbye/node-core").TNullable<string>;
+        email?: import("@botbye/node-core").TNullable<string>;
+        phone?: import("@botbye/node-core").TNullable<string>;
+    };
+    config?: {
+        bypassBotValidation?: import("@botbye/node-core").TNullable<boolean>;
+    };
+    customFields?: {
+        [x: string]: string;
+    };
+} | {
+    type: "risk";
+    request: {
+        ip: string;
+        token?: import("@botbye/node-core").TNullable<string>;
+        requestMethod?: import("@botbye/node-core").TNullable<string>;
+        requestUri?: import("@botbye/node-core").TNullable<string>;
+        headers?: import("@botbye/node-core").TRequestInfo["headers"];
+    } | {
+        request: FastifyRequest<import("fastify").RouteGenericInterface, import("fastify").RawServerDefault, import("http").IncomingMessage, import("fastify").FastifySchema, import("fastify").FastifyTypeProviderDefault, unknown, import("fastify").FastifyBaseLogger, import("fastify/types/type-provider").ResolveFastifyRequestType<import("fastify").FastifyTypeProviderDefault, import("fastify").FastifySchema, import("fastify").RouteGenericInterface>>;
+        token?: import("@botbye/node-core").TNullable<string>;
+    };
+    event: {
+        type: string;
+        status: import("@botbye/node-core").TUpstreamEventStatus;
+    };
+    user: {
+        accountId: string;
+        username?: import("@botbye/node-core").TNullable<string>;
+        email?: import("@botbye/node-core").TNullable<string>;
+        phone?: import("@botbye/node-core").TNullable<string>;
+    };
+    config?: {
+        bypassBotValidation?: import("@botbye/node-core").TNullable<boolean>;
+    };
+    customFields?: {
+        [x: string]: string;
+    };
+    botbyeResult?: string;
+} | {
+    type: "validate";
+    request: {
+        ip: string;
+        headers: Record<string, string>;
+        token?: import("@botbye/node-core").TNullable<string>;
+        requestMethod?: import("@botbye/node-core").TNullable<string>;
+        requestUri?: import("@botbye/node-core").TNullable<string>;
+    } | {
+        request: FastifyRequest<import("fastify").RouteGenericInterface, import("fastify").RawServerDefault, import("http").IncomingMessage, import("fastify").FastifySchema, import("fastify").FastifyTypeProviderDefault, unknown, import("fastify").FastifyBaseLogger, import("fastify/types/type-provider").ResolveFastifyRequestType<import("fastify").FastifyTypeProviderDefault, import("fastify").FastifySchema, import("fastify").RouteGenericInterface>>;
+        token?: import("@botbye/node-core").TNullable<string>;
+    };
+    config?: {
+        bypassBotValidation?: import("@botbye/node-core").TNullable<boolean>;
+    };
+    customFields?: {
+        [x: string]: string;
+    };
+}) => Promise<import("@botbye/node-core").TEvaluationResult>, dev: {
+    setLoggerLevel(level: import("@botbye/node-core").TLoggerLevel): void;
+    sendInitCall(): void;
+    getLogger(): import("@botbye/node-core").TLogger;
+};
+export { init, evaluate, dev, factory, };
+export type * from "@botbye/node-core";
